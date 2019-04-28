4f3f411cc58374c846651b25d53b75f804f63aa4
ThankPan <liuxinghang46@buaa.edu.cn>
2019-04-28 22:19:39 +0800

    爬头像

--- scripts/scrap_author/scrap_author/settings.py
+++ scripts/scrap_author/scrap_author/settings.py
@@ -8,13 +8,14 @@
 #     https://doc.scrapy.org/en/latest/topics/settings.html
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
+import os
 BOT_NAME = 'scrap_author'
 
 SPIDER_MODULES = ['scrap_author.spiders']
 NEWSPIDER_MODULE = 'scrap_author.spiders'
-
-
+IMAGES_STORE = os.path.join(os.path.dirname(os.path.realpath(__file__)),'Avatar')
+IMAGES_URLS_FIELD = 'author.image_urls'
+IMAGES_RESULT_FIELD = 'author.images'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scrap_author (+http://www.yourdomain.com)'
 
@@ -67,8 +68,9 @@ FEED_EXPORT_ENCODING='utf-8'
 ITEM_PIPELINES = {
 #    'scrap_author.pipelines.ScrapAuthorPipeline': 400,
 #    'scrap_author.pipelines.ScrapArticlePipeline':300,
-    'scrap_author.pipelines.AuthorJsonWriterPipeline':200,
-    'scrap_author.pipelines.ArticleJsonWriterPipeline':100,
+    'scrap_author.pipelines.AuthorJsonWriterPipeline':1,
+    'scrap_author.pipelines.ArticleJsonWriterPipeline':1,
+    'scrapy.pipelines.images.ImagesPipeline': 2,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
